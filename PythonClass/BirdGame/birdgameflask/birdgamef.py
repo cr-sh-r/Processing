af0574874d70b2cee899351cd7e2891e5f584e1e
Crshr <clem.hecker@gmail.com>
2025-12-06 19:11:09 -0500

    fix terrible commenting

--- PythonClass/BirdGame/birdgameflask/birdgamef.py
+++ PythonClass/BirdGame/birdgameflask/birdgamef.py
@@ -29,40 +29,42 @@ questions_list = list(questions.keys())
 @app.route("/birdgame")
 @app.route("/birdgame/<gamepage>")
 def game(gamepage = "start"):
+        
         #game is called when the user clicks so it is either the start page or a link which is the answer to the previous page you displayed
 
 ### generates bird list ###
-        qualities_dict = {} # makes an empty dictionary called qualities_list outside the loop  
-        for q in qualities: # loops over the qualities in the list-
-            options_dict= {} # makes temporary dictionary that gets emptied between every quality, stores the options
-            for bird in birds: # loops over each 'bird' in each quality,
-                options_dict[bird[q]] = 1 # within each quality, q is looping over the possible-
-                                          # options for that quality as key with 1 as the value (brown: 1) de-duplicate
-            qualities_dict[q] = list(options_dict.keys()) # lists the keys of options_dict as the value of-
-                                                   # the quality(why its outside the inner loop)
-        #print(qualities_dict)  
+        qualities_dict = {}                                # makes an empty dictionary called qualities_list outside the loop  
+        for q in qualities:                                # loops over the qualities in the list-
+            options_dict= {}                               # makes temporary dictionary that gets emptied between every quality, stores the options
+            for bird in birds:                             # loops over each 'bird' in each quality,
+                options_dict[bird[q]] = 1                  # within each quality, q is looping over the possible-
+                                                           # options for that quality as key with 1 as the value (brown: 1) de-duplicate
+            qualities_dict[q] = list(options_dict.keys())  # lists the keys of options_dict as the value of-
+                                                           # the quality(why its outside the inner loop)
+                                                           #print(qualities_dict)  
 
 ### handle current url ###
-        nextquality = '' #empty string that will hold the quality of the next page of question???
-        answers = {} # gets wiped every time loaded or clicked
+        nextquality = ''                          #empty string that will hold the quality of the next page of question???
+        answers = {}                              # gets wiped every time loaded or clicked
         
         if gamepage == "start": 
-            nextquality = qualities[0] # hard coded to take the first quality on start (color)
-        else: # if the gamepage is not start, assuming no link changing manually, 
-            answers = session['session_answers'] # session is a flask dictionary, 'session_answers' is a key in the dictionary, 
-            #the value is another dictionary we are assigning the inner dictionary to the variable answers
-            answer = request.args.get('answer') # answer is a variable that gets assigned what option was just clicked, 
-            # so the current  url (example):/birdgame/color?answer=white holds through the query paramater the option of the previous question that was chosen
-            answers[gamepage] = answer # takes the answer that we just pulled from the query and puts it as the value of the key 'gamepage' which is the current 
-            #quality that is not the displayed web quality(that technically doesnt exist because we havent sent the new page at this point in the python code or figured out what the nextquality is)
-            # but the one from the url because it is the link itself that will get us to the next page that is holding the information 
-            i = qualities.index(gamepage) # finds where the current quality is in the list of qualities
-            i = i + 1 #incriment to next one in list
-            if i < len(qualities): # if i has not exceeded the list length > if it has, nextqualities stays empty
-                nextquality = qualities[i] # assign nextquality 
+            nextquality = qualities[0]            # hard coded to take the first quality on start (color)
+        else:                                     # if the gamepage is not start, assuming no link changing manually, 
+            answers = session['session_answers']  # session is a flask dictionary, 'session_answers' is a key in the dictionary, 
+                                                  #the value is another dictionary we are assigning the inner dictionary to the variable answers
+            answer = request.args.get('answer')   # answer is a variable that gets assigned what option was just clicked, 
+                                                  # so the current  url (example):/birdgame/color?answer=white holds through the query paramater the option of the previous question that was chosen
+            answers[gamepage] = answer            # takes the answer that we just pulled from the query and puts it as the value of the key 'gamepage' which is the current 
+                                                  #quality that is not the displayed web quality(that technically doesnt exist because we havent sent the new page at this point in the python code or figured out what the nextquality is)
+                                                  # but the one from the url because it is the link itself that will get us to the next page that is holding the information 
+            i = qualities.index(gamepage)         # finds where the current quality is in the list of qualities
+            i = i + 1                             #incriment to next one in list
+            if i < len(qualities):                # if i has not exceeded the list length > if it has, nextqualities stays empty
+                nextquality = qualities[i]        # assign nextquality 
         
-        session['session_answers'] = answers # answers is empty on start, but if it gets a answer from the else block it will be put into session as a value to key session_answers
-        session.modified = True #makes sure flask stores every time?
+        session['session_answers'] = answers      # answers is empty on start, but if it gets a answer from the else block it will be put into session as a value to key session_answers
+        session.modified = True                   #makes sure flask stores every time?
+
         
 ### generate webpage ###      
         output = ""
