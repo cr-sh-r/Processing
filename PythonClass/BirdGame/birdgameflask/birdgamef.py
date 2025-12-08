588c1f8fc84d2308e5ac2a5c48cda71b8b041f85
Crshr <clem.hecker@gmail.com>
2025-12-07 20:46:59 -0500

    cleaning whitespace

--- PythonClass/BirdGame/birdgameflask/birdgamef.py
+++ PythonClass/BirdGame/birdgameflask/birdgamef.py
@@ -1,8 +1,8 @@
 from flask import Flask, session, request
 import json 
 
-app = Flask(__name__) # capital F Flask is the library
-app.secret_key = "birdgamecookies"
+app = Flask(__name__) 
+app.secret_key = "birdgamecookies" #for session cookies 
 
 
 # Open main bird file, read onto variable birdstring
@@ -17,7 +17,7 @@ questions = json.loads(q_string)
 t.close()
 
 
-# create list called qualities taking just the keys from the birds json
+# create list called qualities taking just the keys from the firs bird in birds.json
 qualities = list(birds[0].keys())
 
 # make sure all birds have all qualities and no extra qualities
@@ -32,7 +32,7 @@ for bird in birds:
             print(bird,"has an extra quality:", q)
             exit()
 
-# also make sure there are questions for each quality
+#make sure there are questions for each quality
 
 for q in qualities:
     if q != "name":
@@ -45,8 +45,8 @@ def home():
     output = ""
     question = questions["home"]
     output += f"{question}<br/>"
-    output += f"<a href='/birdgame/start'>yes</a><br/>"
-    output += f"<a href='/nobirdgame'>no</a><br/>"
+    output += "<a href='/birdgame/start'>yes</a><br/>"
+    output += "<a href='/nobirdgame'>no</a><br/>"
     
     return output
     
@@ -58,52 +58,41 @@ def nobird():
 
     return output
 
-    
-
 @app.route("/birdgame/<gamepage>")
 def game(gamepage):
-        
-        
-        
-                #game is called when the user clicks so it is either the start page or a link which is the answer to the previous page you displayed
 
 ### handle current url to get answers up to date ###
-        answers = {}                              # gets wiped every time loaded or clicked
-        
+        answers = {}                              
         if gamepage != "start": 
             answers = session['session_answers']  # session is a flask dictionary, 'session_answers' is a key in the dictionary, 
-                                                  #the value is another dictionary we are assigning the inner dictionary to the variable answers
+                                                  # the value is another dictionary we are assigning the inner dictionary to the variable answers
             answer = request.args.get('answer')   # answer is a variable that gets assigned what option was just clicked, 
                                                   # so the current  url (example):/birdgame/color?answer=white holds through the query paramater the option of the previous question that was chosen
             answers[gamepage] = answer            # takes the answer that we just pulled from the query and puts it as the value of the key 'gamepage' which is the current 
-                                                  #quality that is not the displayed web quality(that technically doesnt exist because we havent sent the new page at this point in the python code or figured out what the nextquality is)
+                                                  # quality that is not the displayed web quality(that technically doesnt exist because we havent sent the new page at this point in the python code or figured out what the nextquality is)
                                                   # but the one from the url because it is the link itself that will get us to the next page that is holding the information 
         
         session['session_answers'] = answers      # answers is empty on start, but if it gets a answer from the else block it will be put into session as a value to key session_answers
         session.modified = True                   #makes sure flask stores every time?
 
 ### filters bird list based on current answers ###
-        # make an empty list for filtered birds
-        # iterate the main birds list
-        # copy birds that pass the answers to the filtered list
 
-        filtered_birds = list(birds)
+        filtered_birds = list(birds) #copies birds 
         # filter a bird if it has the quality but doesn't equal the current answer
         for bird in birds:
-            for q in answers:
+            for q in answers:   # dictionary of qualities and answers from this session
                 if (q,answers[q]) not in bird.items():
                     filtered_birds.remove(bird)
-                    break
+                    break   #breaks from the qualities loop in answers so it doesn't try and remove the same bird again
                     
-        
-        
+### filters qualities based on current answers ###               
         filtered_qualities = list(qualities)
         filtered_qualities.remove("name") 
         for q in qualities:
-            if q in answers.keys():
+            if q in answers:
                 filtered_qualities.remove(q)
 
-
+### de dupes the options for each quality left to ask ##
         qualities_dict = {}                                # makes an empty dictionary called qualities_list outside the loop  
         for q in filtered_qualities:                                # loops over the qualities in the list-
             options_dict= {}                               # makes temporary dictionary that gets emptied between every quality, stores the options
@@ -114,22 +103,16 @@ def game(gamepage):
                                                            # the quality(why its outside the inner loop)
                                                            #print(qualities_dict)  
 
+### sets next quality to ask if there are ones less ##
+        nextquality = ''                          
+        if filtered_qualities: # checks if there is anything left in filtered qualities
+            nextquality = filtered_qualities[0]  
 
-    #get answers up to date
-        nextquality = ''                          #empty string that will hold the quality of the next page of question???
-        if filtered_qualities:
-            nextquality = filtered_qualities[0]
 
-    
-        
-### generate webpage ###      
+### generate webpage for questions and bird guess ###      
         output = ""
-        #output += f"{answers}<br/>"
-        #output += f"{filtered_birds}<br/>"
-        #output += f"{filtered_qualities}<br/>"
-
-        if nextquality:
-            nextquestion = questions[nextquality]
+        if nextquality: # if next quality has any value
+            nextquestion = questions[nextquality] 
             output += f"{nextquestion}<br/>"
             for option in qualities_dict[nextquality]:
                 output += f"<a href='/birdgame/{nextquality}?answer={option}'>{option}</a><br/>"
@@ -139,13 +122,14 @@ def game(gamepage):
         
         return output
 
+### adding a new bird to the data ###
 @app.route('/newbird', methods =["GET", "POST"])
 def newbird():
 
     output = ""
     if request.method == "POST":   #when submit is pushed go into post
         add_bird = {}
-        for q in qualities:
+        for q in qualities:                  ### get answer from form and make dictionary of the new bird
             value = request.form.get(q)
             add_bird[q] = value
 
@@ -154,7 +138,7 @@ def newbird():
 
         for bird in birds:
             match = True
-            for q in check_qualities:
+            for q in check_qualities:            ### checking if the bird entered matches any in the data ###
                 if add_bird[q] != bird[q]:
                     match = False
                     break
@@ -163,24 +147,24 @@ def newbird():
                 output += "<a href='/birdgame'>Back to home</a><br/>"
                 return output
                     
-        birds.append(add_bird)
+        birds.append(add_bird)          # if the bird is not duplicated in the data, append it to the bird list 
         new_birdstr = json.dumps(birds,indent=4)
         b = open("birds.json", "w")
-        b.write(new_birdstr)
+        b.write(new_birdstr)            #write the bird list over the old json file
         b.close()
     
         output = "I have collected the new bird!<br/>"
         output += f"{add_bird}<br/>" 
         output += "<a href='/birdgame'>Back to home</a><br/>"
 
-    else: # GET             # displays form
-        answers = session['session_answers']
+    else: # GET             # displays form for new bird input 
+        answers = session['session_answers'] # get current answers to see what is known about new bird already
         output = "I don't know this bird... you have discovered a new bird! <br/> Name your bird and tell me the other traits"
-        output += '<form action="/newbird" method="post">'
+        output += '<form action="/newbird" method="post">'      #starts the form generating 
         for q in qualities:
             if q in answers:
                 output += f"""
-                    {q} is {answers[q]}
+                    {q} is {answers[q]}                  
                     <input type="hidden" name="{q}" value="{answers[q]}"><br/>
                 """
             else:
